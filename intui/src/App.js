5eb24828cb3de59d5c7dcd1cea45b4a26f4acf00
rajiv <rajiv8589@gmail.com>
2020-02-20 22:54:06 +0530

    initial Ui project setup

---
+++ intui/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function LogRegister() {
+  return (
+    <div className="App">
+      <p>Rajiv Ranjan</p>
+    </div>
+  );
+}
+
+export default LogRegister;
