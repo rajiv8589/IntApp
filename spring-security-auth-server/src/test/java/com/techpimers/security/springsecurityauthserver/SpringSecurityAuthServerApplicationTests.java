d3471e5912ef3975d4271135c00ffacee85fd3c5
Avinash <avinashs763@gmail.com>
2020-02-23 09:59:27 +0530

    Oauth security added/ Swagger added in contract services

---
+++ spring-security-auth-server/src/test/java/com/techpimers/security/springsecurityauthserver/SpringSecurityAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techpimers.security.springsecurityauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
