656c54f8883ab429d5e03ab79089c53c432173ae
Avinash <avinashs763@gmail.com>
2020-02-29 13:41:05 +0530

    security resource serer changes

--- spring-security-auth-server/src/main/java/com/techprimers/security/springsecurityauthserver/SpringSecurityAuthServerApplication.java
+++ spring-security-auth-server/src/main/java/com/techprimers/security/springsecurityauthserver/SpringSecurityAuthServerApplication.java
@@ -1,4 +1,4 @@
-package com.techpimers.security.springsecurityauthserver;
+package com.techprimers.security.springsecurityauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class SpringSecurityAuthServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityAuthServerApplication.class, args);
 	}
-
 }
